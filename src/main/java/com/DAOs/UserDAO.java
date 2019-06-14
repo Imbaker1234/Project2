370aab1deaf8b5d18986161dddeaca7da0d8b4d2
austinbark1 <austinbark1@gmail.com>
2019-06-13 21:22:24 -0400

    wrote get by credentials method using NamedQuery (NamedQuery needs to be modified)

--- src/main/java/com/DAOs/UserDAO.java
+++ src/main/java/com/DAOs/UserDAO.java
@@ -1,5 +1,34 @@
 package com.DAOs;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import com.models.User;
+import com.util.HibernateUtil;
+
 public class UserDAO {
+	
+	public User getUserByCredentials(User logged) {
+		
+		// Establish a session 
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+		// Get the named query 
+		Query query = session.createNamedQuery("findUserByName");
+		query.setParameter("name", logged.getUserName());
+		
+		// Run query and get the result set
+		List<User> loggedIn = query.getResultList();
+		
+		// Close the session
+		session.close();
+		
+		// Return the logged in user
+		return loggedIn.get(0);
+		
+	}
 
 }
