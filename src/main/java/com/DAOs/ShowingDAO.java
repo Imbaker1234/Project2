7670e9fcb0870659faaaea705c8b7d931f75c921
austinbark1 <austinbark1@gmail.com>
2019-06-14 10:31:03 -0400

    added an addShowing method to the ShowingDAO

--- src/main/java/com/DAOs/ShowingDAO.java
+++ src/main/java/com/DAOs/ShowingDAO.java
@@ -1,5 +1,39 @@
 package com.DAOs;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.models.Showing;
+import com.util.HibernateUtil;
+
 public class ShowingDAO {
+	
+	// Create showing
+	public Showing addShowing(Showing newShowing) {
+		
+		// Obtain SessionFactory
+		try (SessionFactory factory = HibernateUtil.getSessionFactory();
+				// Obtain a session
+				Session session = factory.getCurrentSession()) {
+			
+			// Begin a transaction
+			session.beginTransaction();
+			
+			// Add newShowing
+			session.save(newShowing);
+			
+			// Commit the transaction
+			session.getTransaction().commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Return the updated showing
+		return newShowing;
+		
+	}
+	
+	// Read showing
 
 }
