875c48a814e4a6554b5cda082c1429676d61a6c0
austinbark1 <austinbark1@gmail.com>
2019-06-14 22:54:32 -0400

    added login method in the controller

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -1,12 +1,30 @@
 package com.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.models.User;
+import com.services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
 	
+	private UserService us;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.us = userService;
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User login(@RequestBody User logUser) {
+		User loggedUser = us.getByCredentials(logUser);
+		return loggedUser;
+	}
 	
 }
