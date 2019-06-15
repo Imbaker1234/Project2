8d856e7301b888a85e436ea768bcb0ed55093f0f
austinbark1 <austinbark1@gmail.com>
2019-06-14 22:35:26 -0400

    created a UserService class (need to finish)

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -1,8 +1,18 @@
 package com.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.DAOs.UserDAO;
 
 @Service
 public class UserService {
 
+	private UserDAO ud;
+	
+	@Autowired
+	public UserService(UserDAO userDAO) {
+		this.ud = userDAO;
+	}
+	
 }
