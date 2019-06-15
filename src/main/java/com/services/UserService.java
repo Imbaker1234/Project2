d8bf223bce2eca8dbbdbe689e3459d15291305c9
austinbark1 <austinbark1@gmail.com>
2019-06-14 22:48:18 -0400

    modified getByCredentials service

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -2,8 +2,11 @@ package com.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.DAOs.UserDAO;
+import com.models.User;
 
 @Service
 public class UserService {
@@ -13,6 +16,11 @@ public class UserService {
 	@Autowired
 	public UserService(UserDAO userDAO) {
 		this.ud = userDAO;
+	}
+	
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
+	public User getByCredentials(User loginUser) {
+		return ud.getUserByCredentials(loginUser);
 	}
 	
 }
